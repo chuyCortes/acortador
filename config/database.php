e75734d1fc8f5cc16cb753df707195481312aeee
Cortes <chuy_2414@hotmail.es>
2016-03-16 17:52:40 -0600

    add more style

--- config/database.php
+++ config/database.php
@@ -6,6 +6,6 @@
    	define('DB_HOST','localhost'); 
     define('DB_USER','root'); 
     define('DB_PASS','damian'); 
-    define('DB_NAME','firma');
+    define('DB_NAME','urls');
     define('DB_CHARSET','utf-8'); 
 ?>
--- index.php
+++ index.php
@@ -15,11 +15,18 @@
 				<img src="images/logo_dti.png"alt="imagen dti">
 			</header>
 			<main class="main">
-				<p>cortador de url's </p>
-				<input type="text" class="register-input" placeholder="url">
+				<!-- <h3 class="letra">cortador de url's </h3> -->
+				<div id="url">
+					<form action="">
+						<input type="text" class="register-input" placeholder="url">
+						<input type="submit"  value="acortar" class="myButton right" >
+					</form>
+					<p class="short " >La url corta es:<a href="https://bitly.com/">http://uanl.com/<?php echo $master->microurl();?></a></p>
+				</div>
 			</main>
 			<footer>
-				
+				<p class="letra">DIRECCIÓN DE TECNOLOGÍAS DE INFORMACIÓN</p>
+				<p class="letra">UNIVERSIDAD AUTÓNOMA DE NUEVO LEÓN | 2016 Pedro de Alba s/n, San Nicolás de Los Garza, Nuevo León</p>
 			</footer> 
 		</div>
 	</body>
