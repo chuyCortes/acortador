3fa79be5e370c9176e316cd4ba16c5cfc0532c09
Cortes <chuy_2414@hotmail.es>
2016-03-16 16:01:24 -0600

    add some style of index

--- index.php
+++ index.php
@@ -10,6 +10,17 @@
 		<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	</head>
 	<body>
-		<div class="contenedor"></div>
+		<div class="contenedor">
+			<header>
+				<img src="images/logo_dti.png"alt="imagen dti">
+			</header>
+			<main class="main">
+				<p>cortador de url's </p>
+				<input type="text" class="register-input" placeholder="url">
+			</main>
+			<footer>
+				
+			</footer> 
+		</div>
 	</body>
 </html>
