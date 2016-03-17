da7a2a6fd0eb8aa6751e9310f49b370e174163eb
Cortes <chuy_2414@hotmail.es>
2016-03-17 13:00:35 -0600

    working on url cheker

---
+++ home.php
@@ -0,0 +1,69 @@
+<?php
+	include 'core/functions.php';
+	include 'core/querys.php';
+	$master = new fun();
+	$masterQuery = new Querys();
+	if(isset($_POST["submit"]))
+		{
+			if($_POST["urlold"] != ""){
+				$urlVieja = $_POST["urlold"];
+				$urlNueva = "http://uanl.mx/".$master->microurl();
+				$urls = "'".$urlVieja."'".","."'".$urlNueva."'";
+				$urlsSC =$urlVieja.",".$urlNueva;
+				$check = $masterQuery->agregar("urls",$urls,$urlsSC);
+				if($check){
+					$mostrar = true;
+					$new = $masterQuery->newurl();
+					header("Cache-Control: no-cache");
+					header("Expires: -1");
+				}
+				else{ 
+					$mostrar =false;
+					}
+			}
+			else{
+				
+				$mostrar =false;
+			}
+		}
+	else
+	{
+		$mostrar=false;			
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>acortador url</title>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<script src="js/index.js"></script>
+	</head>
+	<body>
+		<div class="contenedor">
+			<header>
+				<img src="images/logo_dti.png"alt="imagen dti">
+			</header>
+			<main class="main">
+				<!-- <h3 class="letra">cortador de url's </h3> -->
+				<div id="url">
+					<form action="" method="post" onsubmit="myFunction();">
+						<input type="text" name="urlold" class="register-input" placeholder="url">
+						<input type="submit" name="submit" value="acortar" class="myButton right" >
+					</form>
+					<?php
+						if($mostrar)
+						{
+							echo " <p class=\"short \" >La url corta es:<a href=\"".$new[1]."\">".$new[2]."</a></p>";
+							$mostrar =false;
+						}
+					?>
+					<!-- <p class="short " >La url corta es:<a href="https://bitly.com/">http://uanl.com/</a></p> -->
+				</div>
+			</main>
+			<footer>
+				<p class="letra">DIRECCIÓN DE TECNOLOGÍAS DE INFORMACIÓN</p>
+				<p class="letra">UNIVERSIDAD AUTÓNOMA DE NUEVO LEÓN | 2016 Pedro de Alba s/n, San Nicolás de Los Garza, Nuevo León</p>
+			</footer> 
+		</div>
+	</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,34 @@
+alert("holis");
+// function isValidUrl(url,obligatory,ftp)
+    function isValidUrl()
+{
+    alert("entro");
+    url = document.getElementById("urlold");
+    obligatory =1;
+    ftp=1;
+    // Si no se especifica el paramatro "obligatory", interpretamos
+    // que no es obligatorio
+    if(obligatory==undefined)
+        obligatory=0;
+    // Si no se especifica el parametro "ftp", interpretamos que la
+    // direccion no puede ser una direccion a un servidor ftp
+    if(ftp==undefined)
+        ftp=0;
+ 
+    if(url=="" && obligatory==0)
+        return true;
+ 
+    if(ftp)
+        var pattern = /^(http|https|ftp)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi;
+    else
+        var pattern = /^(http|https)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi;
+ 
+    if(url.match(pattern))
+        return true; alert("si es url");
+    else
+        return false; alert("no lo es");
+}
+
+function myFunction() {
+    alert("The form was submitted");
+}
